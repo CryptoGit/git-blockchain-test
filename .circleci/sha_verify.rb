695aa7b96cbeac50ad822672e03ac7d7e8772d38
Nathaniel Woodthorpe <d12@github.com>
2018-01-25 11:56:16 -0500

    blah

---
+++ .circleci/sha_verify.rb
@@ -0,0 +1,12 @@
+current_sha = `git rev-parse HEAD`.strip.to_i(16)
+difficulty_sha = `/bin/sh difficulty_factor`.strip.to_i(16)
+
+puts "Block SHA: #{current_sha}"
+puts "Difficulty SHA: #{difficulty_sha}"
+
+if(current_sha < difficulty_sha)
+  puts "Block SHA is good!"
+else
+  puts "Block SHA is not below difficulty SHA"
+  exit 1
+end
